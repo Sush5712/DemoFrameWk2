ed82b46b393852eff8531785400652901fc339eb
lahari <lahari@DESKTOP-3NISN1R>
2024-11-07 10:09:27 +0530

    to pull

---
+++ ActualFrameWk/src/test/java/OrgTest/ToCreateOrgTest.java
@@ -0,0 +1,8 @@
+package OrgTest;
+
+public class ToCreateOrgTest {
+public static void main(String[] args) {
+	
+System.out.println("Org Tesr Created Sucessfully");
+}
+}
---
+++ ActualFrameWk/src/test/java/contsctTest/ToCreateContactTest.java
@@ -0,0 +1,9 @@
+package contsctTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Contact Test Created Successfully");
+	}
+}
